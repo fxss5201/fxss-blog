70845adf40422e1626e3f3845874bb90fcfe03e8
fxss <723107234@qq.com>
2022-11-15 11:19:00 +0800

    feat: 修复埋点多次上报问题

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,10 +1,13 @@
 import { defineClientConfig } from '@vuepress/client'
 
+let path = ''
+
 export default defineClientConfig({
   enhance({ router }) {
     router.beforeEach((to) => {
       if (typeof _hmt !== "undefined") {
-        if (to.path) {
+        if (to.path && path !== to.path) {
+          path = to.path
           _hmt.push(["_trackPageview", to.fullPath])
         }
       }
