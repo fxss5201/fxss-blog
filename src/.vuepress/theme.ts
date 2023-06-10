060efb014155c24bec6fb436e0af97a09002f44f
fxss <723107234@qq.com>
2023-06-10 09:36:52 +0800

    feat: 更新配置

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -124,9 +124,7 @@ export default hopeTheme({
       playground: {
         presets: ["ts", "vue"],
       },
-      presentation: {
-        plugins: ["highlight", "math", "search", "notes", "zoom"],
-      },
+      presentation: ["highlight", "math", "search", "notes", "zoom"],
       stylize: [
         {
           matcher: "Recommended",
