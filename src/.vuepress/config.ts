fd4aae293e36dfccf1df4111b6e137be3de68118
fxss5201 <723107234@qq.com>
2025-04-25 11:30:25 +0800

    feat: 更新文档配置

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,14 +2,11 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 import { registerComponentsPlugin } from '@vuepress/plugin-register-components';
 import { getDirname, path } from "@vuepress/utils";
-import { cut } from "nodejs-jieba";
-import { searchProPlugin } from "vuepress-plugin-search-pro";
-// import { searchPlugin } from "@vuepress/plugin-search";
 
 const __dirname = getDirname(import.meta.url);
 
 export default defineUserConfig({
-  base: "/",
+  base: "/fxss-blog/",
 
   lang: "zh-CN",
   title: "fxss 个人博客",
@@ -41,24 +38,7 @@ export default defineUserConfig({
   plugins: [
     registerComponentsPlugin({
       componentsDir: path.resolve(__dirname, "./components"),
-    }),
-    searchProPlugin({
-      indexContent: true,
-      autoSuggestions: false,
-      indexOptions: {
-        // 使用 nodejs-jieba 进行分词
-        tokenize: (text, fieldName) =>
-          fieldName === "id" ? [text] : cut(text, true),
-      },
     })
-    // searchPlugin({
-    //   locales: {
-    //     '/': {
-    //       placeholder: '搜索',
-    //     },
-    //   },
-    //   hotKeys: ['s', '/', { key: 's', ctrl: true }]
-    // }),
   ],
 
   shouldPrefetch: false,
--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -2,10 +2,10 @@ import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
   "/",
-  { text: "文章列表", icon: "article", link: "/article/" },
-  { text: "文章分类", icon: "categoryselected", link: "/category/javascript/" },
-  { text: "文章标签", icon: "tag", link: "/tag/签字板/" },
-  { text: "时间线", icon: "time", link: "/timeline/" },
+  { text: "文章列表", link: "/article/" },
+  { text: "文章分类", link: "/category/javascript/" },
+  { text: "文章标签", link: "/tag/签字板/" },
+  { text: "时间线", link: "/timeline/" },
   { text: "泛积木-低代码", link: "https://www.fxss.work/" },
   { text: "算法", link: "https://fxss5201.github.io/fxss-leetcode/" },
 ]);
