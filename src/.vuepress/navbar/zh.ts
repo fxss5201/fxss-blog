b33adf6d7ab0b3dde7f92a306665972d6ba36039
fxss <723107234@qq.com>
2026-08-31 22:25:45 +0800

    feat:更新链接

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -6,6 +6,6 @@ export const zhNavbar = navbar([
   { text: "文章分类", link: "/category/javascript/" },
   { text: "文章标签", link: "/tag/签字板/" },
   { text: "时间线", link: "/timeline/" },
-  { text: "泛积木-低代码", link: "https://www.fxss.work/" },
+  { text: "宝宝学习屋", link: "https://learn.fxss.work/" },
   { text: "算法", link: "https://fxss5201.github.io/fxss-leetcode/" },
 ]);
